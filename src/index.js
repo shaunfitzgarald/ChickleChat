e2c16a5b5a7f8ad15c284298a40262f5b8f97e60
John Brady <jbradysd@icloud.com>
2023-10-15 23:38:45 -0700

    added .env file and yarn package DOTENV to the project. Also adjusted the index.js file to hide the sensitive info

--- src/index.js
+++ src/index.js
@@ -12,16 +12,19 @@ import { getAnalytics } from "firebase/analytics";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
+// Load environment variables from .env file
+require('dotenv').config();
+
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "",
+  apiKey: process.env.API_KEY,
   authDomain: "chicklechat.firebaseapp.com",
   projectId: "chicklechat",
   storageBucket: "chicklechat.appspot.com",
-  messagingSenderId: "",
-  appId: "",
-  measurementId: ""
+  messagingSenderId: process.env.MESSAGING_SENDER_ID,
+  appId: process.env.APP_ID,
+  measurementId: process.env.MEASUREMENT_ID
 };
 
 // Initialize Firebase
